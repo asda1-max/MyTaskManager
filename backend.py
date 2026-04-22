969afea1e4f9d31dc670e12215199fbb1665b2b3
Azeroth <rakhathrd1314@gmail.com>
2026-04-22 13:28:03 +0700

    added description, timing update (WIP), and current task delete

--- backend.py
+++ backend.py
@@ -1,16 +1,25 @@
 import sqlite3
 import pathlib
+from datetime import datetime
 
 class task_manager():
-    """_summary_
+    """
+    This Class is for CRUD-ing the task
     """
     def __init__(self):
+        """
+        Class Intitialization :
+            -Creating Database if not existed
+        """
         file_path = pathlib.Path("taskmanager.db")
         if not file_path.exists():
             print("Database existed, making new one")
             self.create_database("taskmanager.db")
 
     def create_database(self, database_name):
+        """
+        Creating Database
+        """
         connection = sqlite3.connect(database=database_name)
         cursor = connection.cursor()
         cursor.execute("""
@@ -33,6 +42,9 @@ class task_manager():
         connection.close()
 
     def add_task(self, task_name, task_type, frequency):
+        """
+        adding task to the databases
+        """
         connection = sqlite3.connect(database="taskmanager.db")
         cursor = connection.cursor()
         cursor.execute("""
@@ -44,6 +56,9 @@ class task_manager():
         connection.close()
 
     def select_task(self,task_type):
+        """
+        selecting task based on task type ('daily', 'weekly', 'monthly')
+        """
         current_data = []
         connection = sqlite3.connect(database="taskmanager.db")
         cursor = connection.cursor()
@@ -59,6 +74,9 @@ class task_manager():
         return current_data
 
     def edit_task(self,id, task_name, task_type, frequency):
+        """
+        edit task
+        """
         connection = sqlite3.connect(database="taskmanager.db")
         cursor = connection.cursor()
         cursor.execute("""
@@ -75,6 +93,9 @@ class task_manager():
         connection.close()
 
     def delete_task(self,id):
+        """
+        delete task
+        """
         connection = sqlite3.connect(database="taskmanager.db")
         cursor = connection.cursor()
         cursor.execute("""
@@ -83,10 +104,17 @@ class task_manager():
                        id = ?
                        
         """,(id,))
+        cursor.execute("""
+            DELETE FROM CURRENT_TASK 
+                    WHERE 
+                       id = ?
+                       
+        """,(id,))
         connection.commit()
         connection.close()
     
     def update_current_task(self,task_type):
+        """updating current task"""
         connection = sqlite3.connect(database="taskmanager.db")
         cursor = connection.cursor()
         data = self.select_task(task_type)
@@ -110,17 +138,36 @@ class task_manager():
         connection.close()
 
     def move_task_to_current_task(self,cursor,id,task_name, task_type, frequency):
+        """
+        moving task from table 'TASK' to 'CURRENT_TASK"
+        """
         cursor.execute("""
                 INSERT INTO CURRENT_TASK(id,task_name, task_type, frequency) VALUES (?,?,?,?)
             """,(id,task_name, task_type, frequency))
+        
+    def timing_update(self):
+        today = datetime.now()
+        print(today.day, today.weekday())
+        if today.day == 1:
+            self.update_current_task('monthly')
+
+        if today.weekday() == 2:
+            self.update_current_task('weekly')
+
+        self.update_current_task('daily')
+
+
     
     def task_logging(self):
+        """
+        WIP
+        """
         pass
 
         
 
 asu = task_manager()
-asu.add_task("eek", "daily", 3)
+asu.delete_task(8)
 # asu.update_current_task("monthly")
 
 
